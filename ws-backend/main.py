c9804dfaf1dd0b21bba14ff94d3e9eb4fe158cac
Srisuntikanaporn <weeravit.srisuntikanaporn@agoda.com>
2022-12-11 01:28:26 +0700

    WIP: change to use Flask

--- ws-backend/main.py
+++ ws-backend/main.py
@@ -1,26 +1,31 @@
-from typing import Union
+from flask import Flask, render_template, request
+from elasticsearch import Elasticsearch
+from dotenv import load_dotenv
+import os
 
-from fastapi import FastAPI
-from pydantic import BaseModel
+load_dotenv()
+api_key_id = os.getenv('API_KEY_ID')
+api_key = os.getenv('API_KEY')
+host = os.getenv('HOST')
 
-app = FastAPI()
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
-
-@app.get("/")
-def health_check():
-    return {"Healthy": "Good!"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+es = Elasticsearch(host, api_key=(api_key_id, api_key))
+app = Flask(__name__)
+picFolder = os.path.join('static', 'img')
+app.config['UPLOAD_FOLDER'] = picFolder
 
 
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+@app.route('/')
+def home():
+    logo = os.path.join(app.config['UPLOAD_FOLDER'], 'GooseGoose.png')
+    return render_template('search.html', user_image=logo)
 
+
+@app.route('/search/results', methods=['POST'])
+def search():
+    search_term = request.form["input"]
+    print(search_term)
+    return render_template('results.html', res=search_term)
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
