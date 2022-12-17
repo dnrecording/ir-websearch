83cf40c670aefea6f02b3a84ce538a5b3eab9741
Srisuntikanaporn <weeravit.srisuntikanaporn@agoda.com>
2022-12-17 21:09:11 +0700

    WIP: able to use simple boolean search

--- ws-backend/dump_data.py
+++ ws-backend/dump_data.py
@@ -5,24 +5,20 @@ from dotenv import load_dotenv
 load_dotenv()
 path = os.getenv('PATH')
 
-category_list = [pos_json for pos_json in os.listdir(
-    path) if pos_json.endswith('.json')]
-
 index_count = 1
-for category in category_list:
-    with open(path+"/"+category) as json_file:
-        tracks = json.load(json_file)
-        for track in tracks["catalogItems"]:
-            head = head = {"index": {"_id": index_count}}
-            data = {
-                "title_id": track["titleId"],
-                "title": track["title"],
-                "boxart":  track["boxart"],
-                "category":  category.replace(".json", "")
-            }
-            index_count += 1
-            with open("movies_list.json", "a") as outfile:
-                json.dump(head, outfile)
-                outfile.write("\n")
-                json.dump(data, outfile)
-                outfile.write("\n")
+with open(path) as json_file:
+    tracks = json.load(json_file)
+    for track in tracks["MoviesList"]:
+        head = head = {"index": {"_id": index_count}}
+        data = {
+            "title_id": track["title_id"],
+            "title": track["title"],
+            "boxart":  track["boxart"],
+            "category":  track["category"]
+        }
+        index_count += 1
+        with open("movies_list.json", "a") as outfile:
+            json.dump(head, outfile)
+            outfile.write("\n")
+            json.dump(data, outfile)
+            outfile.write("\n")
--- ws-backend/main.py
+++ ws-backend/main.py
@@ -8,10 +8,14 @@ api_key_id = os.getenv('API_KEY_ID')
 api_key = os.getenv('API_KEY')
 host = os.getenv('HOST')
 
-es = Elasticsearch(host, api_key=(api_key_id, api_key))
+# es = Elasticsearch(host, api_key=(api_key_id, api_key), verify_certs=False)
+es = Elasticsearch(host, basic_auth=(
+    "elastic", "*wTCURruehG0l*SCdjW8"), verify_certs=False)
+print(es)
 app = Flask(__name__)
 picFolder = os.path.join('static', 'img')
 app.config['UPLOAD_FOLDER'] = picFolder
+# print(es.get(index="movies", id="1"))
 
 
 @app.route('/')
@@ -20,11 +24,23 @@ def home():
     return render_template('search.html', user_image=logo)
 
 
-@app.route('/search/results', methods=['POST'])
+@app.route('/search', methods=['GET', 'POST'])
 def search():
-    search_term = request.form["input"]
-    print(search_term)
-    return render_template('results.html', res=search_term)
+    if request.method == 'POST':
+        query = request.form['query']
+        # print(query)
+        res = es.search(index="movies", body={
+            "query": {
+                "bool": {
+                    "should": [
+                        {"match": {"title": query}}
+                    ]
+                }
+            }
+        })
+        # print(res)
+        return render_template('search_results.html', results=res['hits']['hits'])
+    return render_template('search.html')
 
 
 if __name__ == '__main__':
